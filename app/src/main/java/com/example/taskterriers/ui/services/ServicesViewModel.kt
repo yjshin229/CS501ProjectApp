95a38340ce2b069dbf78266668cae5bf1427cf96
Youngjin <yjshin229@gmail.com>
2023-12-12 02:20:15 -0500

    fetch data from db

--- app/src/main/java/com/example/taskterriers/ui/services/ServicesViewModel.kt
+++ app/src/main/java/com/example/taskterriers/ui/services/ServicesViewModel.kt
@@ -1,10 +1,13 @@
 package com.example.taskterriers.ui.services
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.taskterriers.R
 import com.example.taskterriers.ui.requests.RequestItem
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Locale
@@ -16,25 +19,44 @@ class ServicesViewModel : ViewModel() {
     val serviceTypes = arrayOf("Tutoring", "Move-in", "General", "Personal Care")
     private val dateFormat = SimpleDateFormat("MM-dd-yyyy hh:mm a", Locale.getDefault())
     private val calendar: Calendar = Calendar.getInstance()
+    private var db = Firebase.firestore
 
-    val services = mutableListOf<ServiceCardItem>()
+    private val _services = MutableLiveData<List<ServiceCardItem>>()
+    val services: LiveData<List<ServiceCardItem>> = _services
+    private val firestoreRef = db.collection("services")
+
     init {
-        for(i in 0 until 10){
-            val service = ServiceCardItem(
-                name = "name$i",
-                chipString = serviceTypes[Random.nextInt(4)],
-                descriptionPreview = "This is a description preview$i",
-                numOfReview = Random.nextInt(20),
-                servicePrice = String.format("%.2f",Random.nextDouble(0.0, 100.0)),
-                reviewDecimal = String.format("%.2f",Random.nextDouble(0.0,5.0)),
-                profileImageResId = R.drawable.ic_launcher_background
-            )
-            services += service
+        firestoreRef.get().addOnSuccessListener {documents ->
+            val tempList = mutableListOf<ServiceCardItem>()
+            for(document in documents){
+                val service = ServiceCardItem(
+                    name = document["serviceName"].toString(),
+                    chipString = document["serviceType"].toString(),
+                    descriptionPreview = document["serviceDescription"].toString().substring(0,100),
+                    servicePrice = document["serviceRate"].toString().toInt(),
+                    profileImageResId = R.drawable.ic_launcher_background
+                )
+                tempList.add(service)
+                Log.d("serviceName", document["serviceName"].toString())
+            }
+            _services.value = tempList
+        }
+    }
+    fun refreshData() {
+        firestoreRef.get().addOnSuccessListener { documents ->
+            val tempList = mutableListOf<ServiceCardItem>()
+            for (document in documents) {
+                val service = ServiceCardItem(
+                    name = document["serviceName"].toString(),
+                    chipString = document["serviceType"].toString(),
+                    descriptionPreview = document["serviceDescription"].toString().substring(0,100),
+                    servicePrice = document["serviceRate"].toString().toInt(),
+                    profileImageResId = R.drawable.ic_launcher_background
+                )
+                tempList.add(service)
+            }
+            _services.value = tempList
         }
     }
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
-    }
-    val text: LiveData<String> = _text
 }
